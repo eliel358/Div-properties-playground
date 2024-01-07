d437e96d650256183deb06396607f415b90c7d3e
eliel358 <elielhenrique360flip@gmail.com>
2024-01-07 03:07:43 -0300

    Adição do sistema de temas

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ square = document.getElementById('square')
 square.style.width = String(document.getElementById('range-width').value*0.25)+'em'
 square.style.height = String(document.getElementById('range-height').value*0.25)+'em'
 square.style.rotate = 0
-
+theme = 'dark'
 
 document.getElementById('range-width').addEventListener('input',function(){
     square.style.width = String(document.getElementById('range-width').value*0.25)+'em'
@@ -27,4 +27,19 @@ document.getElementById('range-border-radius-4').addEventListener('input',functi
 })
 document.getElementById('input-backgroundcolor').addEventListener('change',function(x){
     square.style.backgroundColor = document.getElementById('input-backgroundcolor').value
-}) 
+})
+document.getElementById('change-theme').addEventListener('click',function(x){
+    if(theme == 'dark'){
+        theme = 'white'
+        document.getElementById('change-theme').innerHTML = 'Tema preto'
+        document.getElementById('change-theme').className = 'white-theme-button'
+        document.getElementById('square').className = 'square-white'
+        document.body.className = 'body-white'
+        return
+    }
+    theme = 'dark'
+    document.getElementById('change-theme').innerHTML = 'Tema branco'
+    document.getElementById('change-theme').className = 'black-theme-button'
+    document.getElementById('square').className = 'square-dark'
+    document.body.className = 'body-dark'
+})
